32e9ae01d23cd2e95af2496c4179bb22fe93fc69
newstar <kangsaebyeol@gmail.com>
2019-08-03 20:20:34 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,24 +10,31 @@ function stopPainting() {
     painting = false;
 };
 
+function startPainting() {
+    stopPainting();
+};
+
 function onMouseMove(e) {
     const x = e.offsetX;
     const y = e.offsetY;
     // 해당 영역 내에서의 좌표값 : offsetX, Y
     // 전체 window 내에서의 좌표값 : clientX, Y
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 };
 
 function onMouseDown(e) {
     painting = true;
 };
 
-function onMouseUp(e) {
-    painting = false;
-};
-
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
