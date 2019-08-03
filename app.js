208354d46cd9e62fa67a92a1fe8e96f6915030d7
newstar <kangsaebyeol@gmail.com>
2019-08-03 20:55:37 +0900

    Change stroke color

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+
+const colorArray = Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+// Array.from : 유사배열 객체 또는 반복 가능한 객체(iterable object)를 복사해 배열로 생성
+// arrow function 안에 사용된 'color'라는 변수는 각각의 배열 요소를 가리키는 역할을 하는 변수일뿐이라 다른 어떤 이름을 넣어도 상관 없다. 
+
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
 
+function handleColorClick(e) {
+    const changeColor = e.target.style.backgroundColor;
+    ctx.strokeStyle = changeColor;
+};
+
 function stopPainting() {
     painting = false;
 };
 
 function startPainting() {
-    stopPainting();
+    painting = true;
 };
 
 function onMouseMove(e) {
@@ -26,10 +39,6 @@ function onMouseMove(e) {
         ctx.lineTo(x, y);
         ctx.stroke();
     }
-};
-
-function onMouseDown(e) {
-    painting = true;
 };
 
 if(canvas) {
