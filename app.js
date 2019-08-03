b98cbff9edd0a01496e741e764995b9c9af806eb
newstar <kangsaebyeol@gmail.com>
2019-08-03 21:19:38 +0900

    Brush size

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
-
-const colorArray = Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
-// Array.from : 유사배열 객체 또는 반복 가능한 객체(iterable object)를 복사해 배열로 생성
-// arrow function 안에 사용된 'color'라는 변수는 각각의 배열 요소를 가리키는 역할을 하는 변수일뿐이라 다른 어떤 이름을 넣어도 상관 없다. 
+const range = document.getElementById('jsRange');
 
 canvas.width = 700;
 canvas.height = 700;
@@ -14,9 +11,14 @@ ctx.lineWidth = 2.5;
 
 let painting = false;
 
+function handleRangeChange(e) {
+    const size = e.target.value;
+    ctx.lineWidth = size;
+};
+
 function handleColorClick(e) {
-    const changeColor = e.target.style.backgroundColor;
-    ctx.strokeStyle = changeColor;
+    const color = e.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 };
 
 function stopPainting() {
@@ -46,4 +48,13 @@ if(canvas) {
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+}
+
+const colorArray = Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+// Array.from : 유사배열 객체 또는 반복 가능한 객체(iterable object)를 복사해 배열로 생성
+// arrow function 안에 사용된 'color'라는 변수는 각각의 배열 요소를 가리키는 역할을 하는 변수일뿐이라 다른 어떤 이름을 넣어도 상관 없다. 
+
+if(range) {
+    range.addEventListener('change', handleRangeChange);
+    // 강의에서 니꼴라스는 change 대신 input을 사용
 }
